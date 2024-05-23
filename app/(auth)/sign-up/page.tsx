2217d94e6be2bee37d7c6a4ba7daccb5cf911839
plsletmeusethisid <luykim427@gmail.com>
2024-05-23 12:53:36 -0700

    finalize the app

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,8 +2,8 @@ import AuthForm from '@/components/AuthForm'
 
 const SignUp = async () => {
   return (
-    <section className='flex-center size-full max-sm:px-6'>
-      <AuthForm type='sign-up' />
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
     </section>
   )
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,27 +9,23 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   const loggedIn = await getLoggedInUser();
 
   if(!loggedIn) redirect('/sign-in')
 
   return (
     <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn}/>
+      <Sidebar user={loggedIn} />
 
-        <div className="flex size-full flex-col">
-          <div className="root-layout">
-            <Image 
-              src='/icons/logo.svg' width={30} height={30} alt='logo'
-            />
-            <div>
-              <MobileNav user={loggedIn} />
-            </div>
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
           </div>
-          {children}
         </div>
-        
+        {children}
+      </div>
     </main>
   );
 }
